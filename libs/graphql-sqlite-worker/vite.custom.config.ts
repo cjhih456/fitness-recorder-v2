88bfc4f46f9fb065402656450dfaebb78e8b4d41
cjhih456 <cjhih43@gmail.com>
2026-01-09 16:34:38 +0900

    refactor: integrate custom Vite configuration and streamline build process for graphql-sqlite-worker

---
+++ libs/graphql-sqlite-worker/vite.custom.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import GraphqlLoader from 'vite-plugin-graphql-loader';
+import viteWasmPlugin from 'vite-plugin-wasm';
+import * as path from 'path';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  plugins: [
+    viteWasmPlugin(),
+    GraphqlLoader(),
+  ],
+  worker: {
+    format: 'iife' as const,
+    plugins: () => [
+      nxViteTsPaths(),
+      dts({
+        entryRoot: 'src',
+        tsconfigPath: path.join(import.meta.dirname, 'tsconfig.worker.json'),
+        pathsToAliases: false,
+      }),
+      GraphqlLoader(),
+      viteWasmPlugin(),
+    ]
+  },
+  optimizeDeps: {
+    exclude: [
+      '@sqlite.org/sqlite-wasm',
+      'crypto',
+      'os',
+      'zlib'
+    ]
+  },
+})
