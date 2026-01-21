9de7463f406916e3ed4255df2a632dd7f9dbf8aa
cjhih456 <cjhih43@gmail.com>
2026-01-21 15:54:46 +0900

    fix: design

--- app/web/src/components/section/dashboard/todayRoutine/TodayRoutineSection.tsx
+++ app/web/src/components/section/dashboard/todayRoutine/TodayRoutineSection.tsx
@@ -12,11 +12,11 @@ export default function TodayRoutineSection({ data, onClickStartWorkout }: Today
   return (
     <SectionSkeleton title="오늘의 루틴">
       {{
-        default: <>
+        default: <div className="flex flex-col gap-4">
           {data.map(routine => (
             <TodayRoutine key={routine.id} routine={routine} onClickStartWorkout={onClickStartWorkout} />
           ))}
-        </>,
+        </div>,
         subtitle: <span className="text-[10px] text-blue-600 bg-blue-50 px-2 py-1 rounded-full font-bold">5월 20일 월요일</span>
       }}
     </SectionSkeleton>
